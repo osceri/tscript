06bd88a09db7f2b5a702c5317a402820b0ab77e3
osceri <49073891+osceri@users.noreply.github.com>
2022-06-11 16:27:51 +0200

    k

--- turtle.lua
+++ turtle.lua
@@ -1,4 +1,4 @@
-require("lua")
+require("lib")
 
 local modem = peripheral.find("modem") or error("No modem connected")
 modem.open(9999)
@@ -7,6 +7,7 @@ local pos = {}
 
 function get_location()
     local event, side, channel, reply_channel, message, distance
+    modem.transmit(10000, 9999, "ping")
     while true do
         event, side, channel, reply_channel, message, distance = os.pullEvent("modem_message")
         x, _ = parse_location(message)
