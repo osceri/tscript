bc4713a9fe8bec2d06a564fcdcc3584d8b8161ab
osceri <49073891+osceri@users.noreply.github.com>
2022-06-12 14:04:18 +0200

    aaaaa

--- recv_startup.lua
+++ recv_startup.lua
@@ -17,17 +17,17 @@ function ytps(modem)
     local event, side, channel, reply_channel, message, distance
 
     event, side, channel, reply_channel, message, distance = os.pullEvent()
-    modem.callRemote("modem_0", "transmit", 1000, 999, "0")
-    modem.callRemote("modem_1", "transmit", 1000, 999, "1")
-    modem.callRemote("modem_2", "transmit", 1000, 999, "2")
-    modem.callRemote("modem_3", "transmit", 1000, 999, "3")
-    modem.callRemote("modem_4", "transmit", 1000, 999, "4")
+    modem.callRemote("modem_5", "transmit", 1000, 999, "0")
+    modem.callRemote("modem_6", "transmit", 1000, 999, "1")
+    modem.callRemote("modem_7", "transmit", 1000, 999, "2")
+    modem.callRemote("modem_8", "transmit", 1000, 999, "3")
+    modem.callRemote("modem_9", "transmit", 1000, 999, "4")
 end
 
 -- except modem to be attached to modem_0, modem_1 ... modem_4
 local modem = peripheral.find("modem") or error("No modem connected")
 
-modem.callRemote("modem_0", "open", 999)
+modem.callRemote("modem_5", "open", 999)
 
 while true do
     print(os.time())
