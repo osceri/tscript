ed0bf6be83be440a3028f7af1fe9dceac8095341
osceri <49073891+osceri@users.noreply.github.com>
2022-06-12 21:27:24 +0200

    asdasd

--- turtle_sm.lua
+++ turtle_sm.lua
@@ -50,7 +50,6 @@ function x_calibrate(delta)
    end
    if x_calibrate_state == X_CALIBRATE_ then
        if 1 then
-           
            x_calibrate_state = X_CALIBRATE_INIT
            x_calibrate_timer = 0
            return
@@ -59,7 +58,6 @@ function x_calibrate(delta)
    end
    if x_calibrate_state == X_CALIBRATE_END then
        if 1 then
-           
            x_calibrate_timer = x_calibrate_timer + delta
        end
        return
@@ -73,7 +71,6 @@ function x_calibrate(delta)
            return
        end
        if 1 then
-           
            x_goto(delta)
            x_calibrate_timer = x_calibrate_timer + delta
        end
@@ -81,18 +78,14 @@ function x_calibrate(delta)
    end
    if x_calibrate_state == X_CALIBRATE_INIT then
        if 1 then
-           
            x_internal_destination[2] = x_internal_home[2]
-           
            x_internal_destination[1] = position[1]
            x_internal_destination[3] = position[3]
-           
            x_calibrate_state = X_CALIBRATE_GOTO
            x_calibrate_timer = 0
            return
        end
        if 1 then
-           
            x_calibrate_timer = x_calibrate_timer + delta
        end
        return
@@ -108,7 +101,6 @@ function x_goto(delta)
    end
    if x_goto_state == X_GOTO_ then
        if 1 then
-           
            x_goto_state = X_GOTO_INIT
            x_goto_timer = 0
            return
@@ -117,154 +109,120 @@ function x_goto(delta)
    end
    if x_goto_state == X_GOTO_DOWN then
        if 1 then
-           
            x_goto_state = X_GOTO_INIT
            x_goto_timer = 0
            return
        end
        if 1 then
-           
            x_goto_timer = x_goto_timer + delta
        end
        return
    end
    if x_goto_state == X_GOTO_EAST then
        if 1 then
-           
            x_goto_state = X_GOTO_INIT
            x_goto_timer = 0
            return
        end
        if 1 then
-           
            x_goto_timer = x_goto_timer + delta
        end
        return
    end
    if x_goto_state == X_GOTO_INIT then
        if 1 then
-           
            get_location()
-           
            x_goto_state = X_GOTO_MOVE
            x_goto_timer = 0
            return
        end
        if 1 then
-           
            x_goto_timer = x_goto_timer + delta
        end
        return
    end
    if x_goto_state == X_GOTO_MOVE then
        if x_internal_destination[2]>position[2] then
-           
            move('U')
-           
            x_goto_state = X_GOTO_UP
            x_goto_timer = 0
            return
        end
-       if 
-x_internal_destination[2]<position[2] then
-           
-           
+       if x_internal_destination[2]<position[2] then
            move('D')
-           
-           
            x_goto_state = X_GOTO_DOWN
            x_goto_timer = 0
            return
        end
        if x_internal_destination[1]<position[1] then
-           
            move('W')
-           
            x_goto_state = X_GOTO_WEST
            x_goto_timer = 0
            return
        end
        if x_internal_destination[1]>position[1] then
-           
            move('E')
-           
            x_goto_state = X_GOTO_EAST
            x_goto_timer = 0
            return
        end
        if x_internal_destination[3]>position[3] then
-           
            move('S')
-           
            x_goto_state = X_GOTO_SOUTH
            x_goto_timer = 0
            return
        end
-       if 
-x_internal_destination[3]<position[3] then
-           
-           
+       if x_internal_destination[3]<position[3] then
            move('N')
-           
-           
            x_goto_state = X_GOTO_NORTH
            x_goto_timer = 0
            return
        end
        if 1 then
-           
            x_goto_timer = x_goto_timer + delta
        end
        return
    end
    if x_goto_state == X_GOTO_NORTH then
        if 1 then
-           
            x_goto_state = X_GOTO_INIT
            x_goto_timer = 0
            return
        end
        if 1 then
-           
            x_goto_timer = x_goto_timer + delta
        end
        return
    end
    if x_goto_state == X_GOTO_SOUTH then
        if 1 then
-           
            x_goto_state = X_GOTO_INIT
            x_goto_timer = 0
            return
        end
        if 1 then
-           
            x_goto_timer = x_goto_timer + delta
        end
        return
    end
    if x_goto_state == X_GOTO_UP then
        if 1 then
-           
            x_goto_state = X_GOTO_INIT
            x_goto_timer = 0
            return
        end
        if 1 then
-           
            x_goto_timer = x_goto_timer + delta
        end
        return
    end
    if x_goto_state == X_GOTO_WEST then
        if 1 then
-           
            x_goto_state = X_GOTO_INIT
            x_goto_timer = 0
            return
        end
        if 1 then
-           
            x_goto_timer = x_goto_timer + delta
        end
        return
@@ -280,7 +238,6 @@ function x_main(delta)
    end
    if x_main_state == X_MAIN_ then
        if 1 then
-           
            x_main_state = X_MAIN_INIT
            x_main_timer = 0
            return
@@ -289,14 +246,12 @@ function x_main(delta)
    end
    if x_main_state == X_MAIN_CAL then
        if calibrated then
-           
            x_calibrate_state = X_CALIBRATE_
            x_main_state = X_MAIN_DONE
            x_main_timer = 0
            return
        end
        if 1 then
-           
            x_calibrate(delta)
            x_main_timer = x_main_timer + delta
        end
@@ -304,24 +259,20 @@ function x_main(delta)
    end
    if x_main_state == X_MAIN_DONE then
        if 1 then
-           
            x_main_timer = x_main_timer + delta
        end
        return
    end
    if x_main_state == X_MAIN_INIT then
        if 1 then
-           
            x_internal_home = { 40, 40, 40 }
            x_internal_destination = { -1, -1, -1 }
            position = get_location()
-           
            x_main_state = X_MAIN_CAL
            x_main_timer = 0
            return
        end
        if 1 then
-           
            x_main_timer = x_main_timer + delta
        end
        return
