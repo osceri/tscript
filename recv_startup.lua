5a7208dccf41f92e0e88974dac5db7bffa518e95
osceri <49073891+osceri@users.noreply.github.com>
2022-06-12 13:36:09 +0200

    agggg

--- recv_startup.lua
+++ recv_startup.lua
@@ -20,17 +20,10 @@ function ytps()
     print(side, distance)
 end
 
-local modem_0 = peripheral.find("modem_0") or error("No modem_0 connected")
-local modem_1 = peripheral.find("modem_1") or error("No modem_1 connected")
-local modem_2 = peripheral.find("modem_2") or error("No modem_2 connected")
-local modem_3 = peripheral.find("modem_3") or error("No modem_3 connected")
-local modem_4 = peripheral.find("modem_4") or error("No modem_4 connected")
+-- except modem to be attached to modem_0, modem_1 ... modem_4
+local modem = peripheral.find("modem") or error("No modem connected")
 
-modem_0.open(999)
-modem_1.open(999)
-modem_2.open(999)
-modem_3.open(999)
-modem_4.open(999)
+modem.callRemote("modem_0", "open", 999)
 
 while true do
     ytps()
