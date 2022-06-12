1c214715ad06d170b637e01b272b03f9eb635e4c
osceri <49073891+osceri@users.noreply.github.com>
2022-06-12 16:36:58 +0200

    sadasd

--- turtle_startup.lua
+++ turtle_startup.lua
@@ -1,27 +1,6 @@
 require("lib")
 require("math_lib")
 
-local modem = peripheral.find("modem") or error("No modem connected")
-modem.open(9999)
-
-function get_location()
-    local event, side, channel, reply_channel, message, distance
-    modem.transmit(10000, 9999, "ping")
-
-    local d = {}
-    local x = {}
-
-    while true do
-        event, side, channel, reply_channel, message, distance = os.pullEvent("modem_message")
-        d[message] = distance
-        if d['x'] and d['y'] and d['z'] and d['o'] then
-            x = quad(2, d)
-        end
-    end
-
-    return x
-end
-
 while true do
     for i=1,5 do
         get_location()
