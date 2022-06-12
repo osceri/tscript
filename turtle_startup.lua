a6334d5af6da89ec396502bdd2cfa732a6948f48
osceri <49073891+osceri@users.noreply.github.com>
2022-06-12 16:37:46 +0200

    asdadasd

--- turtle_startup.lua
+++ turtle_startup.lua
@@ -3,7 +3,8 @@ require("math_lib")
 
 while true do
     for i=1,5 do
-        get_location()
+        local x = get_location()
+        print(x[1], x[2], x[3])
         turtle.forward()
     end
     turtle.turnLeft()
